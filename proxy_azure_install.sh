3153d3416886a816df8022c5865178ef64b343ef
bharath-zoho <112700052+bharath-zoho@users.noreply.github.com>
2022-09-22 17:10:17 +0530

    Add files via upload
    azure install script

---
+++ proxy_azure_install.sh
@@ -0,0 +1,4 @@
+apt install unzip
+wget https://github.com/bharath-zoho/build_transfer/archive/refs/heads/main.zip
+unzip main.zip
+bash build_transfer-main/Site24x7_Linux_64bit.install -i -key=us_8b24363e42677cb925dc412467cc9659 -mp=20.210.161.202 -fp=20.210.161.202 -gn=azure
